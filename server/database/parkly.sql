dd92c9b24387dbc88a856c66b0c37002d492c717
Alessandro Chitarrini <140112899+chitvs@users.noreply.github.com>
2026-03-25 09:31:56 +0100

    Creazione delle tabelle del database (#40)

--- server/database/parkly.sql
+++ server/database/parkly.sql
@@ -1,13 +1,22 @@
+-- SERIAL: crea un intero auto-incrementante.
+-- NOT NULL: impedisce l'inserimento di valori nulli.
+-- UNIQUE: garantisce che non esistano due record con lo stesso valore per quel campo.
+-- CHECK: applica una regola di validazione logica o di dominio sui dati in ingresso.
+-- DEFAULT: assegna un valore predefinito se non viene specificato durante l'INSERT.
+-- ON DELETE CASCADE: se il record padre viene eliminato, elimina a cascata i record figli collegati.
+
 -- 1. UTENTE
 CREATE TABLE Utente (
     ID_Utente SERIAL PRIMARY KEY,
     Ruolo VARCHAR(10) CHECK (Ruolo IN ('CLIENTE', 'GESTORE')) DEFAULT 'CLIENTE',
     Nome VARCHAR(50) NOT NULL,
     Cognome VARCHAR(50) NOT NULL,
+    CodiceFiscale VARCHAR(16) UNIQUE,
     Email VARCHAR(100) NOT NULL UNIQUE,
     PasswordHash VARCHAR(255) NOT NULL,
     Telefono VARCHAR(20),
-    DataRegistrazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    DataRegistrazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    IsAttivo BOOLEAN DEFAULT TRUE
 );
 
 -- 2. GARAGE  
@@ -15,13 +24,16 @@ CREATE TABLE Garage (
     ID_Garage SERIAL PRIMARY KEY,
     ID_Gestore INT NOT NULL,
     Nome VARCHAR(100) NOT NULL,
+    Descrizione TEXT,
     Indirizzo VARCHAR(150) NOT NULL,
     Latitudine DECIMAL(8,6),
     Longitudine DECIMAL(9,6),   
-    Capienza INT NOT NULL,
-    PrezzoOrario DECIMAL(5, 2) NOT NULL,
+    -- Capienza INT NOT NULL, basta calcolarla con un COUNT(*), è inutile ai fini del database
+    AltezzaMassima DECIMAL(4,2),
+    TariffaBase DECIMAL(5, 2) NOT NULL,
     OrarioApertura TIME NOT NULL,
     OrarioChiusura TIME NOT NULL,
+    Is24h BOOLEAN DEFAULT FALSE, -- Se è TRUE ignoriamo gli orari di apertura e chiusura
     Planimetria_URL VARCHAR(255),
     IsAttivo BOOLEAN DEFAULT TRUE,  
     DataCreazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -38,6 +50,7 @@ CREATE TABLE PostoAuto (
     IsElettrica BOOLEAN DEFAULT FALSE,  
     IsCoperto BOOLEAN DEFAULT TRUE,    
     TariffaOraria DECIMAL(5, 2) NOT NULL,  
+    -- IsDisponibile BOOLEAN DEFAULT TRUE, basta calcolarla con il tempo reale e i parametri InizioSosta e FineSosta della tabella Prenotazione
     FOREIGN KEY (ID_Garage) REFERENCES Garage(ID_Garage) ON DELETE CASCADE,
     UNIQUE(ID_Garage, CodicePosto)
 );
@@ -47,8 +60,13 @@ CREATE TABLE Prenotazione (
     ID_Prenotazione SERIAL PRIMARY KEY,
     ID_Utente INT NOT NULL,  
     ID_Posto INT NOT NULL,
+    CodicePrenotazione VARCHAR(10) UNIQUE,
+    Targa VARCHAR(15),
+    Note TEXT,
     InizioSosta TIMESTAMP NOT NULL,
     FineSosta TIMESTAMP NOT NULL,
+    InizioEffettivo TIMESTAMP DEFAULT NULL,
+    FineEffettiva TIMESTAMP DEFAULT NULL,
     PrezzoTotale DECIMAL(8, 2) NOT NULL,
     Stato VARCHAR(15) CHECK (Stato IN ('ATTIVA', 'ANNULLATA', 'CONCLUSA')) DEFAULT 'ATTIVA',
     DataCreazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
