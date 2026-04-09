53eac2696f26a01134fe812ae6ddea40c5fb0143
Alessandro <alessandro.chitarrini51@gmail.com>
2026-04-09 20:43:13 +0200

    modifica routes

--- server/routes/garage.js
+++ server/routes/garage.js
@@ -1,65 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../database/db');
+const db = require("../database/db");
 
 // ritorna la lista di tutti i garage
-router.get('/', async (req, res) => {
-    try {
-        const garage = await db.any('SELECT * FROM Garage');
-        res.json({ 
-            success: true, 
-            risultati: garage.length,
-            garage 
-        });
-    } catch (err) {
-        console.error('Errore recupero lista garage:', err);
-        res.status(500).json({ 
-            success: false, 
-            error: 'Errore interno'
-        });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const garage = await db.any("SELECT * FROM Garage");
+    res.json({
+      success: true,
+      risultati: garage.length,
+      garage,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: "Errore interno",
+    });
+  }
 });
 
 // dettaglio di un singolo garage
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const garage = await db.oneOrNone('SELECT * FROM Garage WHERE ID_Garage = $1', [id]);
-
-        if (!garage) {
-            return res.status(404).json({ 
-                success: false, 
-                error: 'Garage non trovato' 
-            });
-        }
-
-        res.json({ 
-            success: true, 
-            garage 
-        });
-    } catch (err) {
-        console.error('Errore recupero dettaglio garage:', err);
-        res.status(500).json({ 
-            success: false, 
-            error: 'Errore interno' 
-        });
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const garage = await db.oneOrNone(
+      "SELECT * FROM Garage WHERE ID_Garage = $1",
+      [id],
+    );
+    if (!garage)
+      return res.status(404).json({
+        success: false,
+        error: "Garage non trovato",
+      });
+    res.json({
+      success: true,
+      garage,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: "Errore interno",
+    });
+  }
 });
 
-// stato dei posti per la mappa
-router.get('/:id/posti', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { inizio, fine } = req.query; // date ISO string
+// stato dei posti per la planimetria
+router.get("/:id/posti", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { inizio, fine } = req.query;
 
-        if (!inizio || !fine) {
-            return res.status(400).json({ 
-                success: false, 
-                error: 'Selezionare un intervallo orario' 
-            });
-        }
+    // Anteprima, tutti i posti sono come occupati
+    if (!inizio || !fine || inizio === "" || fine === "") {
+      const querySemplice = `
+                SELECT p.*, FALSE AS is_occupato 
+                FROM PostoAuto p
+                WHERE p.ID_Garage = $1
+                ORDER BY p.CodicePosto
+            `;
+      const posti = await db.any(querySemplice, [id]);
+      return res.json({
+        success: true,
+        posti,
+      });
+    }
 
-        const posti = await db.any(`
+    // Ricerca reale
+    const queryComplessa = `
             SELECT p.*, 
             EXISTS (
                 SELECT 1 FROM Prenotazione pr 
@@ -69,20 +76,20 @@ router.get('/:id/posti', async (req, res) => {
             ) AS is_occupato
             FROM PostoAuto p
             WHERE p.ID_Garage = $1
-        `, [id, inizio, fine]
-        );
-
-        res.json({ 
-            success: true, 
-            posti 
-        });
-    } catch (err) {
-        console.error('Errore recupero mappa posti:', err);
-        res.status(500).json({ 
-            success: false, 
-            error: 'Errore interno' 
-        });
-    }
+            ORDER BY p.CodicePosto
+        `;
+    const posti = await db.any(queryComplessa, [id, inizio, fine]);
+    res.json({
+      success: true,
+      posti,
+    });
+  } catch (err) {
+    console.error("Errore SQL:", err);
+    res.status(500).json({
+      success: false,
+      error: "Errore recupero mappa posti",
+    });
+  }
 });
 
 module.exports = router;
