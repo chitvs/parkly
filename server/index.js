2da947a2ef0466cddfbc3cc00e67c4ab3215a8c7
Andrea Carbone <152490040+andrea222341@users.noreply.github.com>
2026-04-20 11:28:47 +0200

    Update server/index.js
    Co-authored-by: Alessandro Chitarrini <140112899+chitvs@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ const port = process.env.PORT || 3000;
 
 // MIDDLEWARE DI BASE
 app.use(cors({
-    origin: 'http://localhost:5173', // L'indirizzo del tuo frontend
-    credentials: true,               // Permette l'invio dei cookie di sessione
+    origin: 'http://localhost:5173',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
