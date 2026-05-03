84859fdc727afb44d5c7c5483bcec1c80bbfa1cc
Alessandro <alessandro.chitarrini51@gmail.com>
2026-05-03 11:41:18 +0200

    aggiunta tabella recensioni e miglioramento database

--- server/database/parkly.sql
+++ server/database/parkly.sql
@@ -1,11 +1,4 @@
--- SERIAL: crea un intero auto-incrementante.
--- NOT NULL: impedisce l'inserimento di valori nulli.
--- UNIQUE: garantisce che non esistano due record con lo stesso valore per quel campo.
--- CHECK: applica una regola di validazione logica o di dominio sui dati in ingresso.
--- DEFAULT: assegna un valore predefinito se non viene specificato durante l'INSERT.
--- ON DELETE CASCADE: se il record padre viene eliminato, elimina a cascata i record figli collegati.
-
--- 1. UTENTE
+-- UTENTE
 CREATE TABLE Utente (
     ID_Utente SERIAL PRIMARY KEY,
     Ruolo VARCHAR(10) CHECK (Ruolo IN ('CLIENTE', 'GESTORE')) DEFAULT 'CLIENTE',
@@ -16,12 +9,12 @@ CREATE TABLE Utente (
     PasswordHash VARCHAR(255) NOT NULL,
     Telefono VARCHAR(20),
     DataRegistrazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    IsAttivo BOOLEAN DEFAULT TRUE
-    FotoProfilo_URL text DEFAULT NULL;
+    IsAttivo BOOLEAN DEFAULT TRUE,
+    FotoProfilo_URL text DEFAULT NULL,
     Saldo DECIMAL(8, 2) NOT NULL DEFAULT 0.00
 );
 
--- 2. GARAGE  
+-- GARAGE  
 CREATE TABLE Garage (
     ID_Garage SERIAL PRIMARY KEY,
     ID_Gestore INT NOT NULL,
@@ -30,19 +23,18 @@ CREATE TABLE Garage (
     Indirizzo VARCHAR(150) NOT NULL,
     Latitudine DECIMAL(8,6),
     Longitudine DECIMAL(9,6),   
-    -- Capienza INT NOT NULL, basta calcolarla con un COUNT(*), è inutile ai fini del database
     AltezzaMassima DECIMAL(4,2),
     TariffaBase DECIMAL(5, 2) NOT NULL,
     OrarioApertura TIME NOT NULL,
     OrarioChiusura TIME NOT NULL,
-    Is24h BOOLEAN DEFAULT FALSE, -- Se è TRUE ignoriamo gli orari di apertura e chiusura
-    MappaTestuale TEXT, -- Mappa ASCII
+    Is24h BOOLEAN DEFAULT FALSE,
+    MappaTestuale TEXT,
     IsAttivo BOOLEAN DEFAULT TRUE,  
     DataCreazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (ID_Gestore) REFERENCES Utente(ID_Utente) ON DELETE CASCADE
 );
 
--- 3. POSTO AUTO  
+-- POSTO AUTO  
 CREATE TABLE PostoAuto (
     ID_Posto SERIAL PRIMARY KEY,
     ID_Garage INT NOT NULL,
@@ -52,12 +44,11 @@ CREATE TABLE PostoAuto (
     IsElettrica BOOLEAN DEFAULT FALSE,  
     IsCoperto BOOLEAN DEFAULT TRUE,    
     TariffaOraria DECIMAL(5, 2) NOT NULL,  
-    -- IsDisponibile BOOLEAN DEFAULT TRUE, basta calcolarla con il tempo reale e i parametri InizioSosta e FineSosta della tabella Prenotazione
     FOREIGN KEY (ID_Garage) REFERENCES Garage(ID_Garage) ON DELETE CASCADE,
     UNIQUE(ID_Garage, CodicePosto)
 );
 
--- 4. PRENOTAZIONE
+-- PRENOTAZIONE
 CREATE TABLE Prenotazione (
     ID_Prenotazione SERIAL PRIMARY KEY,
     ID_Utente INT NOT NULL,  
@@ -74,3 +65,39 @@ CREATE TABLE Prenotazione (
     FOREIGN KEY (ID_Posto) REFERENCES PostoAuto(ID_Posto) ON DELETE CASCADE,
     CONSTRAINT CHK_DateSosta CHECK (FineSosta > InizioSosta)
 );
+
+-- RECENSIONE
+CREATE TABLE Recensione (
+    ID_Recensione SERIAL PRIMARY KEY,
+    ID_Prenotazione INT NOT NULL UNIQUE, 
+    ID_Utente INT NOT NULL,              
+    ID_Garage INT NOT NULL,              
+    Voto INT NOT NULL CHECK (Voto >= 1 AND Voto <= 5),
+    Commento TEXT,
+    RispostaGestore TEXT, 
+    DataCreazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (ID_Prenotazione) REFERENCES Prenotazione(ID_Prenotazione) ON DELETE CASCADE,
+    FOREIGN KEY (ID_Utente) REFERENCES Utente(ID_Utente) ON DELETE CASCADE,
+    FOREIGN KEY (ID_Garage) REFERENCES Garage(ID_Garage) ON DELETE CASCADE
+);
+
+/*
+
+Note e scelte varie:
+
+> Nella tabella garage non c'è bisogno del campo Capienza, infatti basta calcolarla con un banale COUNT(*). Per quanto riguarda il campo is24h, Se è TRUE ignoriamo del tutto gli orari di apertura e chiusura. Il campo MappaTestuale fa riferimento alla mappa ASCII (planimetria) del garage, che DEVE essere scritto nel formato corretto!
+
+> Nella tabella PostoAuto non serve il campo IsDisponibile, basta vedere il tempo reale e i parametri InizioSosta e FineSosta della tabella Prenotazione
+
+-------------------------------------------
+
+Alcuni significati delle parole chiave sql:
+
+-- SERIAL: crea un intero auto-incrementante.
+-- NOT NULL: impedisce l'inserimento di valori nulli.
+-- UNIQUE: garantisce che non esistano due record con lo stesso valore per quel campo.
+-- CHECK: applica una regola di validazione logica o di dominio sui dati in ingresso.
+-- DEFAULT: assegna un valore predefinito se non viene specificato durante l'INSERT.
+-- ON DELETE CASCADE: se il record padre viene eliminato, elimina a cascata i record figli collegati.
+
+*/
